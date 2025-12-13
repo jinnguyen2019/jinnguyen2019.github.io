a6cf432f506f2362f0a87e221750fa696e9e84c0
jinnguyen <jinnguyen2612@gmail.com>
2025-12-13 13:53:21 +0700

    update
    update

--- js/main.js
+++ js/main.js
@@ -22,3 +22,31 @@ const appearOnScroll = new IntersectionObserver(
 );
 
 faders.forEach(el => appearOnScroll.observe(el));
+
+const toggleBtn = document.getElementById("musicToggle");
+
+toggleBtn.addEventListener("click", () => {
+  if (music.paused) {
+    music.play();
+    toggleBtn.textContent = "🎵";
+  } else {
+    music.pause();
+    toggleBtn.textContent = "🔇";
+  }
+});
+
+document.getElementById("rsvpForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  fetch("https://script.google.com/macros/s/AKfycbyN6BQoqbOI54DW71S0HIJLRGUD2Yw3t3qmiQgKkbbd1oybJ7RTlrUXasY-hMa8oBec3g/exec", {
+    method: "POST",
+    body: JSON.stringify({
+      name: document.getElementById("name").value,
+      status: document.getElementById("status").value
+    })
+  })
+  .then(() => {
+    alert("Cảm ơn bạn đã xác nhận 💙");
+    this.reset();
+  });
+});

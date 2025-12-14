25595ed0e23a5a7fd50a311261fe9433d0e91130
jinnguyen <jinnguyen2612@gmail.com>
2025-12-14 22:43:11 +0700

    update image
    update image

--- js/main.js
+++ js/main.js
@@ -124,7 +124,8 @@ document.addEventListener("DOMContentLoaded", () => {
     "assets/images/3.jpg",
     "assets/images/4.jpg",
     "assets/images/5.jpg",
-    "assets/images/6.jpg"
+    "assets/images/6.jpg",
+    "assets/images/7.jpg"
   ];
 
   let albumIndex = 0;
